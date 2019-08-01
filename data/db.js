f09f89d89e14907ee2e27ac530bc4bea157a5d87
qjgong <qjgong79@gmail.com>
2019-08-01 19:34:58 -0400

    update db

--- data/db.js
+++ data/db.js
@@ -1,14 +1,16 @@
+
 module.exports = function () {
-    // const mongoose = require('mongoose');
-    // var   connectionString =
-    //     'mongodb://localhost:27017/whiteboard';
-    //
-    // if(process.env.MLAB_USERNAME_WEBDEV) {
-    //     var username = process.env.MLAB_USERNAME_WEBDEV;
-    //     var password = process.env.MLAB_PASSWORD_WEBDEV;
-    //     connectionString = 'mongodb://' + username + ':' + password;
-    //     connectionString += '@ds247637.mlab.com:47637/heroku_tvjcd4bj';
-    // }
-    //
-    // mongoose.connect(connectionString,{ useNewUrlParser: true });
+    const mongoose = require('mongoose');
+    var connectionString =
+        'mongodb://localhost:27017/whiteboard';
+
+    if (process.env.MLAB_USERNAME_WEBDEV) {
+        var username = process.env.MLAB_USERNAME_WEBDEV;
+        var password = process.env.MLAB_PASSWORD_WEBDEV;
+        connectionString = 'mongodb://' + username + ':' + password;
+        connectionString += '@ds247637.mlab.com:47637/heroku_tvjcd4bj';
+    }
+
+    mongoose.connect(connectionString, {useNewUrlParser: true});
 };
+
